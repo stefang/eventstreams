47f6d85dfb3819577c5917d0177022e02b551329
Stefan Goodchild <me@stefangoodchild.com>
2010-08-09 15:25:15 +0100

    Adding Edge env

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ set :domain, "eventstreams@eventstreams.triplegeek.com"
 set :deploy_to, "/var/www/eventstreams.triplegeek.com/app/"
 set :repository, 'git@github.com:stefang/eventstreams.git'
 set :revision, 'master' # git branch to deploy
-set :rails_env, 'development'
+set :rails_env, 'edge'
 set :web_command, 'sudo apache2ctl' # command to start/stop apache
