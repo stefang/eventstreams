dbc3c283430692c4bb72542471d30fe5f20e335d
Stefan Goodchild <me@stefangoodchild.com>
2010-08-16 22:13:10 +0100

    Fix: Now works with only one venue item.

--- public/javascripts/eventstreams_gmaps.js
+++ public/javascripts/eventstreams_gmaps.js
@@ -3,33 +3,40 @@ function initialize() {
 	var positions = [];
 	var marker_descriptions = [];
 	
-	
 	$('.location').each(function(i,e){
 		e = $(e);
 		positions.push(new google.maps.LatLng(e.attr('data-lat'), e.attr('data-lng')));
 		marker_descriptions.push(e.attr('data-venue_name'));		
-	})
+	});
 	
-  var myOptions = {
-    zoom: 8,
-    center: positions[0],
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+	if (positions.length > 0) {
+	  var myOptions = {
+	    zoom: 15,
+	    center: positions[0],
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	  };
 
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-	var bounds = new google.maps.LatLngBounds();
+	  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	
-	for (var index in positions) {
-	    bounds.extend(positions[index]);
+		if (positions.length > 1) {
+			var bounds = new google.maps.LatLngBounds();
+			for (var index in positions) {
+			    bounds.extend(positions[index]);
+					new google.maps.Marker({
+					      position: positions[index], 
+					      map: map, 
+					      title: marker_descriptions[index]
+					});
+			}
+			map.fitBounds(bounds);
+		} else {
 			new google.maps.Marker({
-			      position: positions[index], 
+			      position: positions[0], 
 			      map: map, 
-			      title: marker_descriptions[index]
-			});
+			      title: marker_descriptions[0]
+			});			
+		}
 	}
-		
-	map.fitBounds(bounds);
 }
 
 $(document).ready(function(){
