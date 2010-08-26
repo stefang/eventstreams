939b7ae86a46d97b6c830851f6b6621551fddf01
Stefan Goodchild <me@stefangoodchild.com>
2010-08-26 18:07:56 +0100

    Updates for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,16 @@
 set :application, "eventstreams"
-set :domain, "eventstreams@eventstreams.triplegeek.com"
-set :deploy_to, "/var/www/eventstreams.triplegeek.com/app/"
 set :repository, 'git@github.com:stefang/eventstreams.git'
 set :revision, 'master' # git branch to deploy
-set :rails_env, 'edge'
 set :web_command, 'sudo apache2ctl' # command to start/stop apache
+
+task :edge do
+  set :rails_env, 'edge'
+  set :domain, "eventstreams@eventstreams.triplegeek.com"
+  set :deploy_to, "/var/www/eventstreams.triplegeek.com/app/"
+end
+
+task :production do
+  set :rails_env, 'production'
+  set :domain, "eventstreams@eventstreams.triplegeek.com"
+  set :deploy_to, "/var/www/eventstreamsapp.com"
+end
