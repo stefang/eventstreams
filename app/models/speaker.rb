35d892466830da4140da0466c0d2e3d6c027423f
Stefan Goodchild <me@stefangoodchild.com>
2010-09-02 18:40:16 +0100

    Fix: Speaker image upload sized correctly now.

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,7 @@
 class Speaker < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true, :approximate_ascii => true, :strip_non_ascii => true, :scope => :event
   
-  has_attached_file :portrait, :styles => { :medium => "300x300>", :thumb => "100x88>" }
+  has_attached_file :portrait, :styles => { :medium => "300x300", :thumb => "100x88" }
 
   has_and_belongs_to_many :talks
   belongs_to :event
