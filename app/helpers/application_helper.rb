23c665938c033cbc95347b62992ee45a088f37b5
Stefan Goodchild <me@stefangoodchild.com>
2010-09-06 13:34:36 +0100

    [5009886] Event Venues should just be Location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def is_owner?(owned_object)
     current_user == owned_object.user
   end
+  
+  def pluralize_no_number(count, singular, plural = nil)
+     ((count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize))
+  end
 end
