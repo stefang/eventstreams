8830d077656b1e55fd21c5f30415e131706e95b0
Stefan Goodchild <me@stefangoodchild.com>
2010-09-08 15:55:38 +0100

    [4831252] Can delete tweets from admin

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,4 +16,13 @@ class TweetsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @event = current_user.owned_events.find(params[:event_id])
+    @tweet = Tweet.find(params[:id], :scope => @event)
+    @tweet.destroy
+    flash[:notice] = "Successfully destroyed tweet."
+    redirect_to user_event_tweets_path(current_user, @event)
+  end
+
 end
