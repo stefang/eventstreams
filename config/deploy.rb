cb1ab9501575ff46f4770dc215f89005d1a58298
Stefan Goodchild <me@stefangoodchild.com>
2010-09-18 17:38:17 +0100

    Updated and working deploy hacks

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,19 +7,27 @@ set :deploy_timestamped, false
 task :edge do
   set :rails_env, 'edge'
   set :domain, "eventstreams@eventstreams.triplegeek.com"
+  set :my_deploy_path, "/var/www/eventstreams.triplegeek.com/app/"
   set :deploy_to, "/var/www/eventstreams.triplegeek.com/app/"
 end
 
 task :production do
   set :rails_env, 'production'
   set :domain, "eventstreams@eventstreams.triplegeek.com"
+  set :my_deploy_path, "/var/www/eventstreamsapp.com/app/"
   set :deploy_to, "/var/www/eventstreamsapp.com/app/"
 end
 
 desc "Full deployment cycle"
-    task "vlad:deploy" => %w[
-      vlad:update
-      vlad:migrate
-      vlad:start_app
-      vlad:cleanup
-    ]
+namespace :vlad do
+  remote_task :manual_migrate do
+      run "cd #{my_deploy_path}current && /usr/bin/rake db:migrate RAILS_ENV=#{rails_env}"
+  end
+  remote_task :symlink_assets do
+      run "ln -s #{my_deploy_path}shared/event_assets #{my_deploy_path}current/assets/event_assets"
+  end
+  remote_task :custom_restart do
+      run "touch #{my_deploy_path}current/tmp/restart.txt"
+  end
+  remote_task :deploy => [:update, :manual_migrate, :symlink_assets, :cleanup, :custom_restart]
+end
