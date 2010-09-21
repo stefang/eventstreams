b6a84e62dfc54f006f6ede46562128d5c7c723d0
Stefan Goodchild <me@stefangoodchild.com>
2010-09-21 23:22:35 +0100

    [5282139] For speaker portrait image, helpful to suggest max resolution eg 150px x 150px or whatever

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,12 +1,15 @@
 class Speaker < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true, :approximate_ascii => true, :strip_non_ascii => true, :scope => :event
   
-  has_attached_file :portrait, :styles => { :medium => "300x300#", :thumb => "100x88#" }, 
+  has_attached_file :portrait, :styles => { :original => "980x980", :medium => "300x300#", :thumb => "100x88#" }, 
     :path => ":rails_root/assets/event_assets/:event_id/:attachment/:id/:style/:filename",
     :url => "/event_assets/:event_id/:attachment/:id/:style/:filename"
 
   has_and_belongs_to_many :talks
   belongs_to :event
+
+  validates_attachment_size :portrait, :less_than=>1.megabyte
+  validates_attachment_content_type :portrait, :content_type=>['image/jpeg', 'image/png', 'image/gif']
 
   validates_presence_of :name, :on => :create
   validates_presence_of :event_id, :on => :create
