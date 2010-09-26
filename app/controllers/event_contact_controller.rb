118bd3a2830dd89a3067eee7c893acf0b82f80a3
Stefan Goodchild <me@stefangoodchild.com>
2010-09-27 00:28:16 +0100

    Fix: Prevent blank messages from being sent.

--- app/controllers/event_contact_controller.rb
+++ app/controllers/event_contact_controller.rb
@@ -6,12 +6,17 @@ class EventContactController < ApplicationController
   
   def create
     get_published_or_owned_event
-    if Notification.deliver_contact(params, @event)
-      flash[:notice] = "Email was successfully sent."
-      redirect_to(contact_path)
+    if params[:message].blank? || params[:name].blank? || params[:email].blank?
+      flash.now[:error] = "You must provide a name, email address and a message."
+      render :layout => 'event', :action => :index      
     else
-      flash.now[:error] = "An error occurred while sending this email."
-      render :index
+      if Notification.deliver_contact(params, @event)
+        flash[:notice] = "Email was successfully sent."
+        redirect_to(contact_path)
+      else
+        flash.now[:error] = "An error occurred while sending this email."
+        render :layout => 'event', :action => :index    
+      end
     end
   end  
 end
