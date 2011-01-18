0fa3785b83f3781e96a9acf46aa214bcd808df41
Stefan Goodchild <me@stefangoodchild.com>
2011-01-18 16:45:14 +0000

    Intersections time format change, new page titles and style support

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,7 +2,7 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
   :dmy_long => Proc.new { |date| "#{date.day.ordinalize} #{date.strftime '%B %Y'}" },
   :dm_long => Proc.new { |date| "#{date.day.ordinalize} #{date.strftime '%B'}" },
   :form_display => "%d-%m-%Y",
-  :programme_header => Proc.new { |date| "#{date.strftime '%A'} #{date.day.ordinalize} #{date.strftime '%B'}" }
+  :programme_header => Proc.new { |date| "<span class='day'>#{date.strftime '%A'}</span> <span class='date'>#{date.day.ordinalize} #{date.strftime '%B'}</span>" }
   
 )
 
