f8d05babc07ed4bba50642ee5d0b6ba7d11fa8bc
Stefan Goodchild <me@stefangoodchild.com>
2011-07-18 18:50:41 +0100

    Remove unused user model fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,9 @@
 class User < ActiveRecord::Base
   include Clearance::User
-  has_attached_file :avatar, :styles => { :medium => "176x176#", :thumb => "88x88#", :tiny => "22x22#" }, 
-    :path => ":rails_root/public/system/:attachment/:id/:style/:id.jpg", 
-    :url => "/system/:attachment/:id/:style/:id.jpg",
-    :default_url => '/images/default_avatar.jpg'
 
   has_many :owned_events, :class_name => 'Event', :foreign_key => :user_id, :dependent => :destroy
 
-  attr_accessible :first_name
-  attr_accessible :last_name
-  attr_accessible :nickname
-  attr_accessible :avatar
-  
   attr_readonly :superadmin
-  
-  validates_presence_of :first_name, :on => :create
-  validates_presence_of :last_name, :on => :create
   
   def superadmin?
     superadmin
