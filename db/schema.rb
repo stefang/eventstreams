a1be7c6b2d7b43c88b351d0c25ba038117f7f1da
Stefan Goodchild <me@stefangoodchild.com>
2011-07-18 18:50:55 +0100

    Updated scheme.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110622144631) do
+ActiveRecord::Schema.define(:version => 20110715124117) do
 
   create_table "event_assets", :force => true do |t|
     t.string   "title"
@@ -188,21 +188,14 @@ ActiveRecord::Schema.define(:version => 20110622144631) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "encrypted_password",  :limit => 128
-    t.string   "salt",                :limit => 128
-    t.string   "token",               :limit => 128
+    t.string   "encrypted_password", :limit => 128
+    t.string   "salt",               :limit => 128
+    t.string   "token",              :limit => 128
     t.datetime "token_expires_at"
-    t.boolean  "email_confirmed",                    :default => false, :null => false
+    t.boolean  "email_confirmed",                   :default => false, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "superadmin",                         :default => false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "nickname"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.boolean  "superadmin",                        :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
